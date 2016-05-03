63c18deaa445b4f247f578eb73cd67c136ad1918
Zahid Islam <zahiddevs@gmail.com>
2016-05-03 17:15:57 -0400

    Initial Commit
    Version 1.0.1 (RC #1)

---
+++ js/background.js
@@ -0,0 +1,61 @@
+var isNotified = false;
+setInterval(statusChecker,60000);
+statusChecker();
+
+function statusChecker() {
+    var request = new XMLHttpRequest();
+    request.open("GET", "https://api.twitch.tv/kraken/streams/angelskimi", true);
+
+    request.onreadystatechange = function() {
+        if (request.readyState == 4 && request.status == 200) {
+            var twitchAPI = JSON.parse(request.responseText);
+            var online = twitchAPI.stream !== null ? true : false;
+
+            if (online) {
+                if (!isNotified) {
+                    isNotified = true;
+                    notification();
+                }
+                chrome.browserAction.setIcon({
+                    path: "../img/icons/kimi_on_19.png"
+                });
+                chrome.browserAction.setTitle({
+                    title: "AngelsKimi is Live!"
+                });
+            } else {
+                isNotified = false;
+                chrome.browserAction.setIcon({
+                    path: "../img/icons/kimi_off_19.png"
+                });
+                chrome.browserAction.setTitle({
+                    title: "Angelskimi is Offline!"
+                });
+            }
+        }
+    };
+    request.send();
+}
+
+function notification() {
+    var options = {
+        "type": "basic",
+        "iconUrl": "../img/icons/icon_128.png",
+        "title": "AngelsKimi Stream Status",
+        "message": "AngelsKimi is now ONLINE!",
+        "buttons": [{
+            "title": "Go to AngelsKimi's stream!",
+            "iconUrl": "../img/social/twitch.png"
+        }],
+        "isClickable": false,
+        "requireInteraction": true,
+    };
+
+    chrome.notifications.create("kimiStream", options, function(notificationId) {});
+}
+
+chrome.notifications.onButtonClicked.addListener(function(notificationId) {
+    chrome.tabs.create({
+        url: "https://www.twitch.tv/angelskimi"
+    });
+    chrome.notifications.clear(notificationId);
+});
---
+++ js/panel.js
@@ -0,0 +1,29 @@
+function statusChecker() {
+    var request = new XMLHttpRequest();
+    request.open("GET", "https://api.twitch.tv/kraken/streams/angelskimi", true);
+
+    request.onreadystatechange = function() {
+        if (request.readyState == 4 && request.status == 200) {
+            var twitchAPI = JSON.parse(request.responseText);
+            var online = twitchAPI.stream !== null ? true : false;
+            var portrait = document.getElementById("kimi_angel");
+
+            if (online) {
+                document.getElementById("kimi-status").innerHTML = "AngelsKimi is Live!";
+                portrait.src = "../img/kimi_angel.png";
+                document.getElementById('preview').style.backgroundImage = "url('https://static-cdn.jtvnw.net/previews-ttv/live_user_angelskimi-640x360.jpg')";
+
+            } else {
+                document.getElementById("kimi-status").innerHTML = "AngelsKimi is Offline!";
+                portrait.src = "../img/Kimi_angel_sad.png";
+                document.getElementById("preview").style.backgroundImage = "url('../img/background/offline_preview.jpg')";
+            }
+        }
+    };
+    request.send();
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    setInterval(statusChecker,60000);
+    statusChecker();
+});
