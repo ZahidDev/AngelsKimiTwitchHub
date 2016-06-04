f8033dc34cd179383f9a4dac3de7c59eee77decd
ZahidDev <zahiddevs@gmail.com>
2016-06-04 07:53:07 -0400

    Revert "Add range for more frequent online checks."
    This reverts commit 3215b05fbe3be2ac662af05556ebe2793fd6bb8b.

--- js/panel.js
+++ js/panel.js
@@ -24,24 +24,6 @@ function statusChecker() {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  var offset;
-  var currentDate = new Date();
-  var junOffset = new Date(currentDate.getFullYear(), 6, 1).getTimezoneOffset();
-
-  if (currentDate.getTimezoneOffset() == junOffset)
-      offset = -7;
-  else
-      offset = -8;
-
-  var LADate = new Date( new Date().getTime() + offset * 3600 * 1000).toUTCString().replace( / GMT$/, "" );
-  var LADateArray = LADate.split(" ");
-  var LATime = LADateArray[4];
-
-  if((LATime >= "16:00:00") && (LATime <= "20:30:00")){
-    setInterval(statusChecker,25000);
-    statusChecker();
-  } else{
     setInterval(statusChecker,60000);
     statusChecker();
-  }
 });
